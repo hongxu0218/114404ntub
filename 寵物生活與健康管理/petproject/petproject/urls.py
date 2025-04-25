ab57df10a132ab48af90bc25b562eed280f4074a
ngiam1002 <168848644+ngiam1002@users.noreply.github.com>
2025-04-25 17:32:39 +0800

    寵物生活與健康管理/petproject/petproject---settings/urls.py

--- 寵物生活與健康管理/petproject/petproject/urls.py
+++ 寵物生活與健康管理/petproject/petproject/urls.py
@@ -1,24 +1,44 @@
-# petproject/urls.py
+"""
+URL configuration for ntub114404 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
 
+from petapp import views    #
 from django.urls import include
-from petapp import views
-from petapp.views import CustomSignupView
-
+from django.views.generic import TemplateView
+from allauth.account.views import LoginView
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # 首頁
 
-    path('accounts/', include('allauth.urls')),  # 登入、註冊、密碼等功能
-    path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
-    path('accounts/select-type/', views.select_type_then_social_login, name='select_type_then_social'),
+    path('', views.home, name='home'),  # 根路徑顯示首頁
+
+    path('register/', include('petapp.urls')),
+    path('accounts/', include('allauth.urls')),  # 加入 allauth 的帳號處理路由
     path('accounts/logout-success/', views.logout_success, name='logout_success'),
-
-    path('', include('petapp.urls')),  # 引入 petapp 自訂功能路由（例如註冊）
+    # path('listone/',views.listone),
+    # path('listall/',views.listall),
+    # path('',views.index),
+    # path('index/', views.index),
+    path('', include('petapp.urls')),
 ]
 
 if settings.DEBUG:
